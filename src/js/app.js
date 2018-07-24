dc203e31f1c1130c2ccd16c9cc6ba798e7fc4177
Dillon Browne <dillonbrowne@live.com>
2018-07-24 16:41:48 -0600

    convert material icons to package

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
 // JS Goes here - ES6 supported
+import $ from "jquery";
+window.jQuery = $;
+window.$ = $;
 import "material-kit/assets/js/core/bootstrap-material-design.min";
 import "material-kit/assets/js/material-kit";
 import "smoothscroll";
