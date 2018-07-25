5b910c16be112d97187cfe9278aeed46033d4c4b
Dillon Browne <dillonbrowne@live.com>
2018-07-24 22:41:53 -0600

    notify

--- src/js/app.js
+++ src/js/app.js
@@ -7,5 +7,42 @@ import "material-kit/assets/js/material-kit";
 import "smoothscroll";
 import "@fortawesome/fontawesome-free/js/all";
 import "material-design-icons";
+import "bootstrap-notify";
 // Say hello
 console.log("🦊 Hello!");
+
+$("#EmailMe").submit(function(e) {
+  e.preventDefault();
+  var $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function() {
+    msgSent();
+  });
+});
+$("#TextMe").submit(function(e) {
+  e.preventDefault();
+  var $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function() {
+    msgSent();
+  });
+});
+
+function msgSent() {
+  $.notify({
+    // options
+    message: "Thanks for reaching out to me! I will contact you within the next day.",
+    icon: "fa fa-check-circle",
+  }, {
+    // settings
+    placement: {
+      from: "bottom",
+      align: "center"
+    },
+    type: "success",
+    timer: 1000,
+    animate: {
+      enter: "animated fadeInDown",
+      exit: "animated fadeOutUp"
+    },
+  });
+}
+
