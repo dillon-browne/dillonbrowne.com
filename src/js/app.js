de2550b18b7222f73302099741609c09db2a77e6
Dillon Browne <dillonbrowne@live.com>
2018-09-17 16:40:35 -0600

    fix scroll

--- src/js/app.js
+++ src/js/app.js
@@ -26,19 +26,19 @@ var typedDesktop = new Typed("#typedDesktop", {
   loop: true
 });
 
-var granimInstance = new Granim({
-  element: "#granim-canvas",
-  name: "granim",
-  opacity: [1, 1],
-  states : {
-    "default-state": {
-      gradients: [
-        ["#834D9B", "#D04ED6"],
-        ["#1CD8D2", "#93EDC7"]
-      ]
-    }
-  }
-});
+// var granimInstance = new Granim({
+//   element: "#granim-canvas",
+//   name: "granim",
+//   opacity: [1, 1],
+//   states : {
+//     "default-state": {
+//       gradients: [
+//         ["#834D9B", "#D04ED6"],
+//         ["#1CD8D2", "#93EDC7"]
+//       ]
+//     }
+//   }
+// });
 console.log(granimInstance);
 console.log(typed);
 console.log(typedDesktop);
